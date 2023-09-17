1a37a2a8a84f16b60739c1daa88fe5d525e583b6
Mirsidiq <mirzokirov2002@gmail.com>
2023-09-17 15:58:46 +0500

    add toggle menu

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,18 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from "react-router-dom";
+import HeaderLogo from "../../assets/images/header-logo.svg"
 import "./header.scss"
 export default function Header() {
+    const [dropdown,setDropDown]=useState(false)
+    const [toggle,setToggle]=useState(false)
   return (
     <header className='header'>
         <div className="container">
             <div className="nav">
                 <NavLink href="#" className="header__logo">
-                    <span className='header__logo__text'>L</span>ight<span className='header__logo__text'>S</span>chool
+                    <span className='header__logo__img'></span>
+                    <span className='header__logo__img'></span>
+                    <span className='header__logo__text'>Light School</span>
                 </NavLink>
-                <ul className='nav__list'> 
-                <li className='nav__item'>
+                <ul className={`nav__list ${toggle ? 'nav__list__active':''}`}> 
+                <span className='header__toggle__close' onClick={()=>setToggle(false)}></span>
+                <li className={'nav__item'} onClick={()=>(setDropDown(!dropdown))} >
                         Kategoriyalar
-                        <ul className='nav__inner__list'>
+                        <ul className={`nav__inner__list ${ dropdown ?'nav__inner__list__active':""}`}>
                             <NavLink className="nav__item__inner">
                                 Darslar
                             </NavLink>
@@ -28,6 +34,11 @@ export default function Header() {
                         Bog'lanish
                     </NavLink>
                 </ul>
+                <div className="header__toggle" onClick={()=>setToggle(true)}>
+                    <span className='header__toggle__line'></span>
+                    <span className='header__toggle__line'></span>
+                    <span className='header__toggle__line'></span>
+                </div>
             </div>
         </div>
     </header>
