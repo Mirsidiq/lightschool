5a9c7412ebf5f3d1a55391b393c257a5e352db7c
Mirsidiq <mirzokirov2002@gmail.com>
2023-09-20 09:44:34 +0500

    add slider to main page

--- src/pages/hero/Hero.jsx
+++ src/pages/hero/Hero.jsx
@@ -1,12 +1,19 @@
-import React, { useEffect,useState } from 'react'
+import React, { useEffect,useState,useRef } from 'react'
 import { TypeAnimation } from 'react-type-animation';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import Aos from 'aos'
-import './hero.scss'
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
 import 'aos/dist/aos.css'
+import './hero.scss'
+import { EffectCoverflow } from 'swiper/modules';
 import Card from "../../components/card/Card"
 import MainBg from "../../assets/images/hero-bg.png"
 import VideoIcon from "../../assets/images/video.svg"
+import Teacher from '../../assets/images/teacher.png'
 import { NavLink } from 'react-router-dom'
+import Button from '../../components/button/Button';
 export default function Hero() {
   const popular =[
     {
@@ -62,11 +69,11 @@ export default function Hero() {
       </section>
       <section className="services">
         <div className="container">
-          <h3 className="services__title" data-aos="fade-up" data-aos-delay="300">
+          <h3 className="services__title" data-aos="fade-up" data-aos-delay="1000">
             Bizning xizmatlarimiz
           </h3>
           <div className="services__inner">
-              <div className="service__card" data-aos="flip-left" data-aos-delay="700">
+              <div className="service__card" data-aos="flip-left" data-aos-delay="1200">
                 <div className="service__card__header">
                 <span className='service__card__header__icon__wrapper'>
                 <i className='service__card__header__icon fa-solid fa-laptop fa-bounce'></i>
@@ -78,7 +85,7 @@ export default function Hero() {
                 <NavLink className='service__card__more__btn'>Ko'proq <i className="fa-solid fa-chevron-right fa-beat service__card__more__btn-arrow"></i></NavLink>
                 </div>
               </div>
-              <div className="service__card" data-aos="flip-right" data-aos-delay="800" data-aos-duration="700">
+              <div className="service__card" data-aos="flip-right" data-aos-delay="1400" data-aos-duration="700">
                 <div className="service__card__header">
                 <span className='service__card__header__icon__wrapper'>
                 <i className='service__card__header__icon fa-solid fa-gamepad fa-shake'></i>
@@ -95,15 +102,107 @@ export default function Hero() {
       </section>
       <section className="popular">
         <div className="container">
-          <h3 className="popular__title" data-aos="fade-down" data-aos-delay="600">
+          <h3 className="popular__title" data-aos="fade-down" data-aos-delay="1000">
             Top darslar
           </h3>
           <div className="popular__inner">
            {
             popular.map(e=>(
-              <Card data={e} aos={{"data-aos":"zoom-in","data-aos-delay":"800"}}/>
+              <Card data={e} aos={{"data-aos":"zoom-in","data-aos-delay":"1400"}}/>
             ))
            }
+          </div>
+          <div className="popular__footer">
+            <Button className="popular__footer__btn">Barcha darslar</Button>
+          </div>
+        </div>
+      </section>
+      <section className="teachers">
+        <div className="container">
+          <h3 className='services__title teachers__title'>Bizning ustozlar</h3>
+          <div className="teachers__inner">
+          <Swiper
+        effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={'auto'}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+       modules={[EffectCoverflow]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+        <div className="teacher__card">
+              <div className="teacher__header">
+                  <img src={Teacher} alt="teacher image" className='teacher__img' />
+              </div>
+              <div className="teacher__body">
+                  <h4 className='teacher__name'>Mirsidiq Mirzokirov</h4>
+                  <p className='teacher__profession'>Informatika o'qituvchisi</p>
+              </div>
+            </div>
+        </SwiperSlide>
+        <SwiperSlide>
+        <div className="teacher__card">
+              <div className="teacher__header">
+                  <img src={Teacher} alt="teacher image" className='teacher__img' />
+              </div>
+              <div className="teacher__body">
+                  <h4 className='teacher__name'>Mirsidiq Mirzokirov</h4>
+                  <p className='teacher__profession'>Informatika o'qituvchisi</p>
+              </div>
+            </div>
+        </SwiperSlide>
+        <SwiperSlide>
+        <div className="teacher__card">
+              <div className="teacher__header">
+                  <img src={Teacher} alt="teacher image" className='teacher__img' />
+              </div>
+              <div className="teacher__body">
+                  <h4 className='teacher__name'>Mirsidiq Mirzokirov</h4>
+                  <p className='teacher__profession'>Informatika o'qituvchisi</p>
+              </div>
+            </div>
+        </SwiperSlide>
+        <SwiperSlide>
+        <div className="teacher__card">
+              <div className="teacher__header">
+                  <img src={Teacher} alt="teacher image" className='teacher__img' />
+              </div>
+              <div className="teacher__body">
+                  <h4 className='teacher__name'>Mirsidiq Mirzokirov</h4>
+                  <p className='teacher__profession'>Informatika o'qituvchisi</p>
+              </div>
+            </div>
+        </SwiperSlide>
+        <SwiperSlide>
+        <div className="teacher__card">
+              <div className="teacher__header">
+                  <img src={Teacher} alt="teacher image" className='teacher__img' />
+              </div>
+              <div className="teacher__body">
+                  <h4 className='teacher__name'>Mirsidiq Mirzokirov</h4>
+                  <p className='teacher__profession'>Informatika o'qituvchisi</p>
+              </div>
+            </div>
+        </SwiperSlide>
+        <SwiperSlide>
+        <div className="teacher__card">
+              <div className="teacher__header">
+                  <img src={Teacher} alt="teacher image" className='teacher__img' />
+              </div>
+              <div className="teacher__body">
+                  <h4 className='teacher__name'>Mirsidiq Mirzokirov</h4>
+                  <p className='teacher__profession'>Informatika o'qituvchisi</p>
+              </div>
+            </div>
+        </SwiperSlide>
+      </Swiper>
           </div>
         </div>
       </section>
