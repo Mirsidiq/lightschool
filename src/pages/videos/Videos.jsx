09fd0f5dc5c91bb7cc8f6135432e5b50187819ec
Mirsidiq <mirzokirov2002@gmail.com>
2023-09-24 10:26:56 +0500

    make videos page responsive

--- src/pages/videos/Videos.jsx
+++ src/pages/videos/Videos.jsx
@@ -3,7 +3,7 @@ import ReactPlayer from 'react-player';
 import "./videos.scss"
 import { NavLink, useParams } from 'react-router-dom';
 export default function Videos() {
-    let {id}=useParams()
+    let {id}=useParams() 
     const src =
   "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4"
     return (
@@ -11,6 +11,38 @@ export default function Videos() {
         <div className="container">
         <NavLink className="lessons__back back" to={`/lessons/${id}`}><i class="fa-solid fa-chevron-left fa-beat back__chevron"></i>Ortga</NavLink>
             <div className="videos__inner">
+                <div className="video__card">
+                    <div className="video__card__header">
+                        <video src={src} controls className='video__card__content'></video>
+                    </div>
+                    <div className="video__card__body">
+                        <h4 className="video__card__title">Ko'phadlar va ular ustida amallar qo'shish ko'paytirish bo'lish</h4>
+                    </div>
+                </div>
+                <div className="video__card">
+                    <div className="video__card__header">
+                        <video src={""} controls className='video__card__content'></video>
+                    </div>
+                    <div className="video__card__body">
+                        <h4 className="video__card__title">Ko'phadlar va ular ustida amallar qo'shish ko'paytirish bo'lish</h4>
+                    </div>
+                </div>
+                <div className="video__card">
+                    <div className="video__card__header">
+                        <video src={src} controls className='video__card__content'></video>
+                    </div>
+                    <div className="video__card__body">
+                        <h4 className="video__card__title">Ko'phadlar va ular ustida amallar qo'shish ko'paytirish bo'lish</h4>
+                    </div>
+                </div>
+                <div className="video__card">
+                    <div className="video__card__header">
+                        <video src={src} controls className='video__card__content'></video>
+                    </div>
+                    <div className="video__card__body">
+                        <h4 className="video__card__title">Ko'phadlar va ular ustida amallar qo'shish ko'paytirish bo'lish</h4>
+                    </div>
+                </div>
                 <div className="video__card">
                     <div className="video__card__header">
                         <video src={src} controls className='video__card__content'></video>
